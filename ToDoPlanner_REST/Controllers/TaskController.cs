aa4608392ac440650c770b12c1858b23d90cb12e
David I <davidibanezmoya@gmail.com>
2025-05-19 19:01:50 +0200

    REST project created and apps configured

---
+++ ToDoPlanner_REST/Controllers/TaskController.cs
@@ -0,0 +1,7 @@
+﻿namespace ToDoPlanner_REST.Controllers
+{
+    public class TaskController
+    {
+        //Control of the different methods
+    }
+}
---
+++ ToDoPlanner_REST/Models/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoPlanner_REST.Models
+{
+    public class Task
+    {
+        //Attributes that the Tasks will have
+        private int Id { get; set; }
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private int Status { get; set; }
+    }
+}
